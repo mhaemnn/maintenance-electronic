4330a3dcb44b7b281c323a1a976feba4030630a9
mhaemnn <muhaemin1998@gmail.com>
2023-10-16 10:25:05 +0700

    membuat folder operasi aritmatika

---
+++ #5_operMatika/aritmatika.py
@@ -0,0 +1,42 @@
+# Belajar operasi aritmatika
+
+
+# operasi tambah +
+a = 10
+b = 20
+hasil = a + b
+print(a, '+', b, '=', hasil)
+
+# operasi pengurangan -
+hasil = a - b
+print(a, '-', b, '=', hasil)
+
+# operasi perkalian *
+hasil = a * b
+print(a, '*', b, '=', hasil)
+
+# operasi pengurangan /
+hasil = a / b
+print(a, '/', b, '=', hasil)
+
+# operasi ekspnen (pangkat)
+hasil = a ** b
+print(a, '**', b, '=', hasil)
+
+# operasai modulus % (hasil bagi)
+hasil = a % b
+print(a, '%', b, '=', hasil)
+
+# operasi floor devision (pembulatan)
+hasil = a // b
+print(a, '//', b, '=', hasil)
+
+
+# prioritas operasi
+"""
+1. ()
+2. exponen **
+3. * / ** % //
+4. pertambahan dan pengurangan + - 
+
+"""
