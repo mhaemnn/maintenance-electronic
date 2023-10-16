a45c66cf0ef752ec6008a5d1ec9af7acef624a03
mhaemnn <muhaemin1998@gmail.com>
2023-10-16 09:26:32 +0700

    menambahkan folder casting tipe data

---
+++ #3_catingTipedata/casting.py
@@ -0,0 +1,27 @@
+# CASTING = Merubah satu tipe data ke tipe data lain
+
+
+# TIPE DATA INTEGER ->
+print("=======INTEGER========")
+data_int = 9
+print('data = ', data_int, 'bertipe = ', type(data_int))
+
+
+data_float = float(data_int)
+data_str = str(data_int)
+data_bool = bool(data_int)
+print('data = ', data_float, 'bertipe = ', type(data_float))
+print('data = ', data_str, 'bertipe = ', type(data_str))
+print('data = ', data_bool, 'bertipe = ', type(data_bool))
+
+# TIPE DATA FLOAT ->
+print("=======FLOAT========")
+data_float = 20.1
+print('data = ', data_float, 'bertipe = ', type(data_float))
+
+data_int = int(data_float)
+data_str = str(data_float)
+data_bool = bool(data_float)
+print('data = ', data_int, 'bertipe = ', type(data_float))
+print('data = ', data_str, 'bertipe = ', type(data_str))
+print('data = ', data_bool, 'bertipe = ', type(data_bool))
