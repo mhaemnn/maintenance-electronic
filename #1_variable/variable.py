5cb3c776bb78b05fe9441e7f6f0b935cb45c099d
mhaemnn <muhaemin1998@gmail.com>
2023-10-16 08:32:07 +0700

    belajar variable python

--- #1_variable/variable.py
+++ #1_variable/variable.py
@@ -1,2 +1,11 @@
-print("hahhah")
-print("hello python")
+# variable adalah tempat menyimpan data
+angka1 = 10
+angka2 = 100
+
+print("nilai angka1 = ", angka1)
+print("nilai angka2 = ", angka2)
+
+
+# assigment indirec
+ankga3 = angka1
+print("nilai angka3 = ", ankga3)
