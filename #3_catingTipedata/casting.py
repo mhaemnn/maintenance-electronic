e383d7f586881d0eab829ff6415c67534e8c7fd8
mhaemnn <muhaemin1998@gmail.com>
2023-10-24 20:48:58 +0700

    modifikasi file casting.py

--- #3_catingTipedata/casting.py
+++ #3_catingTipedata/casting.py
@@ -3,9 +3,8 @@
 
 # TIPE DATA INTEGER ->
 print("=======INTEGER========")
-data_int = 9
-print('data = ', data_int, 'bertipe = ', type(data_int))
-
+data_int = 8
+print("data = ", data_int, "bertipe = ", type(data_int))
 
 data_float = float(data_int)
 data_str = str(data_int)
@@ -13,6 +12,7 @@ data_bool = bool(data_int)
 print('data = ', data_float, 'bertipe = ', type(data_float))
 print('data = ', data_str, 'bertipe = ', type(data_str))
 print('data = ', data_bool, 'bertipe = ', type(data_bool))
+
 
 # TIPE DATA FLOAT ->
 print("=======FLOAT========")
