88ff600276cc606509f33c8fbaaead3cab6cf192
mhaemnn <muhaemin1998@gmail.com>
2023-10-25 09:17:31 +0700

    modifikasi file latihanLogika.py

--- #9_latihan_komLogika/latihanLogika.py
+++ #9_latihan_komLogika/latihanLogika.py
@@ -1,1 +1,12 @@
-# Belajar Latihan logika
+# Belajar Latihan logika dan komparasi
+user = float(input(
+    "MASUKAN ANGKA YANG BERNILAI \n kurang dari 3 \n atua \n lebih dari 10 \n :"))
+
+kurang = (user < 3)
+print("kurang dari 3: ", kurang)
+
+lebihDari = (user > 10)
+print("lebih darii 10:", lebihDari)
+
+iscorect = kurang or lebihDari
+print("angka yang ada masukan: ", iscorect)
