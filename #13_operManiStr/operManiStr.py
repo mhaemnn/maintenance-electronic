ca8ded68decb648574df5ddb80e8e47241c48123
mhaemnn <muhaemin1998@gmail.com>
2023-10-26 10:10:02 +0700

    menambahkan folder operasi manipulasi string part2

---
+++ #13_operManiStr/operManiStr.py
@@ -0,0 +1,12 @@
+# Belajar operator manipulasi string
+# merubah semua ke uppercase dan lowercase
+salam = "hello"
+print('normal', salam)
+
+salam = salam.upper()
+print('upper: ', salam)
+
+salam = salam.lower()
+print('lower: ', salam)
+
+print('\n', 40 * '=', '\n')
