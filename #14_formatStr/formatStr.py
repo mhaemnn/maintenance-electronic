264c98d8aa7a4d47a7e963a03576f24b5e6557da
mhaemnn <muhaemin1998@gmail.com>
2023-10-27 19:56:16 +0700

    modifikasi

--- #14_formatStr/formatStr.py
+++ #14_formatStr/formatStr.py
@@ -1,3 +1,4 @@
+# contoh generic
 nama = "muhaemin"
 format_str = f"hello: {nama}"
 print(format_str)
@@ -5,5 +6,24 @@ print(format_str)
 
 # menggunakan boolean
 bool = True
-bool_str = f"hello: {bool}"
-print(bool_str)
+format_str = f"hello: {bool}"
+print(format_str)
+
+# angka
+angka = 2005.5
+format_str = f"angka: {angka}"
+print(format_str)
+
+# bilangan bulat
+angka = 12
+format_str = f"angka: {angka: d}"
+print(format_str)
+
+# angka ribuan / jutaan
+angka = 2000000
+format_str = f"angka: {angka}"
+print(format_str)
+
+# bilangan decimal
+angka = 20054.32445
+format_str = f"angka: {angka: }"
