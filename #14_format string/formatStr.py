c0825bd8dc88fe9c9dc15eabcff44eda408f9bb9
mhaemnn <muhaemin1998@gmail.com>
2023-10-27 20:41:43 +0700

    rename name all folder

---
+++ #14_format string/formatStr.py
@@ -0,0 +1,34 @@
+# contoh generic
+nama = "muhaemin"
+format_str = f"hello: {nama}"
+print(format_str)
+
+
+# menggunakan boolean
+bool = True
+format_str = f"hello: {bool}"
+print(format_str)
+
+# angka
+angka = 2005.5
+format_str = f"angka: {angka}"
+print(format_str)
+
+# bilangan bulat
+angka = 12
+format_str = f"angka: {angka: d}"
+print(format_str)
+
+# angka ribuan / jutaan
+angka = 2000000
+format_str = f"angka: {angka}"
+print(format_str)
+
+# bilangan decimal
+angka = 20054.32445
+format_str = f"angka: {angka: 010.37}"
+print(format_str)
+
+persentase = 0.034
+format_persen = f"persen: {persentase: %}"
+print(format_persen)
---
+++ #4_input data/input.py
@@ -0,0 +1,14 @@
+# Belajar input data dari user
+
+data = input("Masukan nama = ")
+print('data', data, 'bertipe = ', type(data))
+
+# mengambil angak int & float
+angka = int(input("Masukan angka int = "))
+angka = float(input("Masukan angak float ="))
+print('data', angka, 'bertipe = ', type(angka))
+
+
+# mengambail boolean
+biner = bool(int(input("Masukan nilai bool = ")))
+print('data', biner, 'bertipe = ', type(biner))
